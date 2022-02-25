458c22b0f2c82bf445d4f25e9996476959df48d1
Yufeng Li <2592737L@student.gla.ac.uk>
2022-02-25 21:49:54 +0000

    chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     }
 }
 
-
+LOGIN_URL = 'rango:login'
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -102,6 +102,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 
 # Internationalization
